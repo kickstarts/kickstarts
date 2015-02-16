a8db17bdac3e945979d14c41f33d30f43bce4893
Vitor Britto <code@vitorbritto.com.br>
2015-02-15 21:27:30 -0300

    Update General Template

--- init/templates/general/includes/partials/footer.php
+++ init/templates/general/includes/partials/footer.php
@@ -23,17 +23,12 @@
 
     </script>
 
-    <?php
-        $localhost = $_SERVER['SERVER_NAME'];
-        if ($localhost === 'localhost') :
-    ?>
-    <!-- Browser Sync -->
-    <script type='text/javascript' id="__bs_script__">
-    //<![CDATA[
-    document.write("<script async src='//HOST:3000/browser-sync/browser-sync-client.1.7.0.js'><\/script>".replace(/HOST/g, location.hostname).replace(/PORT/g, location.port));
-    //]]>
-    </script>
-    <?php endif; ?>
+    <?php if ('localhost' === $_SERVER['SERVER_NAME']): ?>
+    <!-- Live Reload - DEVELOPMENT ONLY -->
+    <script type='text/javascript' id="__bs_script__">//<![CDATA[
+    document.write("<script async src='//HOST:3000/browser-sync/browser-sync-client.1.9.1.js'><\/script>".replace(/HOST/g, location.hostname).replace(/PORT/g, location.port));
+//]]></script>
+    <?php endif;?>
 
 
 </body>
--- init/templates/general/includes/partials/header.php
+++ init/templates/general/includes/partials/header.php
@@ -1,21 +1,24 @@
-<!doctype html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<!DOCTYPE html>
+<html class="no-js" lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Project Title</title>
+    <meta charset="utf-8">
+    <meta name="robots" content="noindex,nofollow">
+
+    <title></title>
+
+    <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" /><![endif]-->
+
     <meta name="description" content="">
     <meta name="keywords" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="google-site-verification" content="" />
-    <meta name="viewport" content="width=device-width">
+    <link type="text/plain" rel="author" href="humans.txt" />
 
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/apple-144.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/apple-114.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/apple-72.png">
-    <link rel="apple-touch-icon-precomposed" href="assets/apple-57.png">
-    <link rel="shortcut icon" href="assets/favicon.png">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/images/apple-144.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/apple-114.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/apple-72.png">
+    <link rel="apple-touch-icon-precomposed" href="assets/images/apple-57.png">
+    <link rel="shortcut icon" href="assets/images/favicon.png">
 
     <link rel="stylesheet" href="assets/styles/style.min.css">
     <script src="assets/scripts/vendor/modernizr-2.6.2.min.js"></script>
