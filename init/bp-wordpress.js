b44892b54ba410b965daa7b639b0162015666d06
Vitor Britto <code@vitorbritto.com.br>
2015-08-22 14:39:32 -0300

    Update scaffolding and log messages

--- init/bp-wordpress.js
+++ init/bp-wordpress.js
@@ -15,9 +15,9 @@ var nodeCheck = sh.which('node'),
     gitCheck  = sh.which('git');
 
 // Paths
-var themePath      = './wp-content/themes/default/',
-    pluginsPath    = './wp-content/plugins/'
-    repositoryPath = 'git clone https://github.com';
+var themePath   = './wp-content/themes/default/',
+    pluginsPath = './wp-content/plugins/',
+    gitClone    = 'git clone https://github.com';
 
 
 
@@ -26,11 +26,7 @@ var themePath      = './wp-content/themes/default/',
 
 sh.echo(info('→ Initializing...'));
 
-sh.echo(info('→ Creating Structure'));
 sh.rm('-rf', ['./node_modules', './.git', '.gitignore', '.editorconfig', '.travis.yml', 'newproject.js', 'package.json', 'README.md', 'logo-bp.jpg']);
-
-sh.echo(info('→ Creating Documentation'));
-sh.mkdir('./docs');
 
 // Check if Git exists
 if (!gitCheck) {
@@ -39,36 +35,81 @@ if (!gitCheck) {
 } else {
 
     // Install WordPress
-    sh.exec(repositoryPath + '/WordPress/WordPress');
+    sh.echo(info('→ Installing WordPress...'));
+    sh.exec(gitClone + '/WordPress/WordPress');
     sh.mv('./WordPress/*', './');
     sh.rm('-rf', ['./WordPress']);
+    sh.echo(done('✔ WordPress successfully installed!'));
+
+    // Scaffolding
+    sh.echo(info('→ Creating Structure...'));
+    sh.mkdir('-p' themePath);
     sh.mv('./init/templates/wordpress/*', themePath);
-    sh.rm('-rf', ['./init']);
     sh.mv(themePath + '__wp-config.txt', './wp-config.php');
     sh.mv(themePath + '__gitignore.txt', './.gitignore');
     sh.mv(themePath + '__htaccess.txt', themePath + 'htaccess.txt');
+    sh.rm('-rf', ['./init']);
+    sh.echo(done('✔ Created!'));
 
     // Install WP Plugins
-    sh.exec(repositoryPath + '/Automattic/jetpack.git', pluginsPath);
-    sh.exec(repositoryPath + '/elliotcondon/acf.git', pluginsPath);
-    sh.exec(repositoryPath + '/Yoast/wordpress-seo.git', pluginsPath);
-    sh.exec(repositoryPath + '/Yoast/google-analytics-for-wordpress.git', pluginsPath);
-    sh.exec(repositoryPath + '/wp-plugins/admin-menu-editor.git', pluginsPath);
-    sh.exec(repositoryPath + '/wp-plugins/contact-form-7.git', pluginsPath);
-    sh.exec(repositoryPath + '/wp-plugins/w3-total-cache.git', pluginsPath);
-    sh.exec(repositoryPath + '/wp-plugins/reduce-bounce-rate.git', pluginsPath);
+    sh.echo(info('→ Installing Plugins...'));
+
+    sh.echo(info('→ Clonning Jetpack...'));
+    sh.exec(gitClone + '/Automattic/jetpack.git');
+    sh.mv('./jetpack', pluginsPath);
+    sh.echo(done('✔ Jetpack successfully cloned!'));
+
+    sh.echo(info('→ Clonning Advanced Custom Fields...'));
+    sh.exec(gitClone + '/elliotcondon/acf.git');
+    sh.mv('./jetpack', pluginsPath);
+    sh.echo(done('✔ Advanced Custom Fields successfully cloned!'));
+
+    sh.echo(info('→ Clonning Yoast...'));
+    sh.exec(gitClone + '/Yoast/wordpress-seo.git');
+    sh.mv('./wordpress-seo', pluginsPath);
+    sh.echo(done('✔ Yoast successfully cloned!'));
+
+    sh.echo(info('→ Clonning Google Analytics for WordPress...'));
+    sh.exec(gitClone + '/Yoast/google-analytics-for-wordpress.git');
+    sh.mv('./google-analytics-for-wordpress', pluginsPath);
+    sh.echo(done('✔ Google Analytics for WordPress successfully cloned!'));
+
+    sh.echo(info('→ Clonning Admin Menu Editor...'));
+    sh.exec(gitClone + '/wp-plugins/admin-menu-editor.git');
+    sh.mv('./admin-menu-editor', pluginsPath);
+    sh.echo(done('✔ Admin Menu Editor successfully cloned!'));
+
+    sh.echo(info('→ Clonning Contact Form 7...'));
+    sh.exec(gitClone + '/wp-plugins/contact-form-7.git');
+    sh.mv('./contact-form-7', pluginsPath);
+    sh.echo(done('✔ Contact Form 7 successfully cloned!'));
+
+    sh.echo(info('→ Clonning W3 Total Cache...'));
+    sh.exec(gitClone + '/wp-plugins/w3-total-cache.git');
+    sh.mv('./w3-total-cache', pluginsPath);
+    sh.echo(done('✔ W3 Total Cache successfully cloned!'));
+
+    sh.echo(info('→ Clonning Reduce Bounce Rate...'));
+    sh.exec(gitClone + '/wp-plugins/reduce-bounce-rate.git');
+    sh.mv('./reduce-bounce-rate', pluginsPath);
+    sh.echo(done('✔ Reduce Bounce Rate successfully cloned!'));
+
+    sh.echo(done('✔ Plugins successfully installed!'));
 }
 
 
 // Setup
 // ----------------------------------
 
-sh.echo(info('→ Setting up project'));
+sh.echo(info('→ Setting up project...'));
 
 // Grant Permission
+sh.echo(info('→ Setting permission to folders and files...'));
 chmod('-R 755', './');
+sh.echo(done('✔ Done!'));
 
 // Move on to "./src" folder
+sh.echo(info('→ Installing dependencies....'));
 sh.cd('./src');
 
 // Check if NodeJS exists and install dependencies
@@ -77,8 +118,10 @@ if (!nodeCheck) {
     process.exit(1);
 } else {
     sh.exec('npm install');
+    sh.echo(done('✔ Node Modules successfully installed!'));
+
 }
 
 // Final Message
 sh.echo(done('✔ All Done!'));
-sh.echo(info('\n→ Remember to configure your "wp_config.php" file'));
+sh.echo(warn('\n!!! Remember to setup your "wp_config.php" file'));
