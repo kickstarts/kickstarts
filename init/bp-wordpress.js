7b15712e2ce24e0fb970f808a965c623cacd7895
Vitor Britto <code@vitorbritto.com.br>
2016-06-20 10:34:43 -0300

    Fix Clone Path

--- init/bp-wordpress.js
+++ init/bp-wordpress.js
@@ -20,7 +20,7 @@ var themePath   = './wp-content/themes/default/',
     pluginsPath = './wp-content/plugins/',
     gitClone    = 'git clone https://github.com',
     svnClone    = 'svn checkout http://plugins.svn.wordpress.org/',
-    wpRepo      = 'WordPress/WordPress',
+    wpRepo      = 'https://github.com/WordPress/WordPress',
     wpBranch    = '4.4-branch',
     twentyList  = [
         './wp-content/themes/twentyten',
@@ -47,7 +47,7 @@ if (!gitCheck) {
 
     // Install WordPress
     sh.echo(info('→ Installing WordPress...'));
-    sh.exec('git clone -b ' + wpBranch + '/' + wpRepo);
+    sh.exec('git clone -b ' + wpBranch + wpRepo);
     sh.mv('./WordPress/*', './');
     sh.rm('-rf', ['./WordPress']);
     sh.echo(done('✔ WordPress successfully installed!'));
